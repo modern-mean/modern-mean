1d0747330e03dfb8f93c12739ecc101a488fc165
Andrew Throener <athroener@gmail.com>
2016-04-22 16:17:09 -0500

    Cleanup

--- tasks/gulp/old/jshint.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-  jshint = require('gulp-jshint');
-
-gulp.task('jshint', function () {
-  return gulp.src(['modules/**/*.js', 'config/**/*.js', 'tasks/**/*.js'])
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
-});
--- tasks/gulp/old/stripDebug.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-  stripDebug = require('gulp-strip-debug');
-
-gulp.task('stripDebug', function () {
-  return gulp.src('public/dist/**/application.js')
-    .pipe(stripDebug())
-    .pipe(gulp.dest('public/dist'));
-});
